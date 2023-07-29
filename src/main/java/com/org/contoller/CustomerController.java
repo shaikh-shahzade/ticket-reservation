22897b7ede9e32ef58942364a610930e2505e01f
shaikh shahzade <shahzade10@gmail.com>
2023-07-29 23:58:15 +0530

    Customer Controller: Api

--- src/main/java/com/org/contoller/CustomerController.java
+++ src/main/java/com/org/contoller/CustomerController.java
@@ -1,4 +1,40 @@
 package com.org.contoller;
 
+import com.org.model.Customer;
+import com.org.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("customer")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+    @GetMapping
+    public List<Customer> getAllCustomers()
+    {
+        return customerService.getAllCustomers();
+    }
+    @GetMapping("{id}")
+    public Customer getCustomer(@PathVariable Long id)
+    {
+        return customerService.getCustomers(id);
+    }
+    @PostMapping
+    public Customer addCustomer(@RequestBody Customer customer)
+    {
+        return customerService.getCustomers(id);
+    }
+    @PutMapping
+    public Customer updateCustomer(@RequestBody Customer customer)
+    {
+        return customerService.updateCustomers(id);
+    }
+    @DeleteMapping
+    public Customer deleteCustomer(@RequestBody Long id)
+    {
+        return customerService.deleteCustomers(id);
+    }
 }
