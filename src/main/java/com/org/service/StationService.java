27e4501e816e0f2c10d05e6026828ac90d30b070
shaikh shahzade <shahzade10@gmail.com>
2023-08-03 23:32:41 +0530

    Station Service Impl

--- src/main/java/com/org/service/StationService.java
+++ src/main/java/com/org/service/StationService.java
@@ -1,4 +1,17 @@
 package com.org.service;
 
+import com.org.model.Station;
+
+import java.util.List;
+
 public interface StationService {
+    Station getStationById(Long id);
+
+    List<Station> getAllStation();
+
+    Station addStatioon(Station station);
+
+    Station updateStation(Station station);
+
+    Station deleteStation(Station station);
 }
--- src/main/java/com/org/serviceimpl/StationServiceImpl.java
+++ src/main/java/com/org/serviceimpl/StationServiceImpl.java
@@ -1,4 +1,35 @@
 package com.org.serviceimpl;
 
-public class StationServiceImpl {
+import com.org.model.Station;
+import com.org.service.StationService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StationServiceImpl implements StationService {
+    @Override
+    public Station getStationById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Station> getAllStation() {
+        return null;
+    }
+
+    @Override
+    public Station addStatioon(Station station) {
+        return null;
+    }
+
+    @Override
+    public Station updateStation(Station station) {
+        return null;
+    }
+
+    @Override
+    public Station deleteStation(Station station) {
+        return null;
+    }
 }
